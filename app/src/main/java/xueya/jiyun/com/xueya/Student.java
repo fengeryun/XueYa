db4772459f5f66596818be1b3a3c3bd2ea770cbf
hyj1245593267 <1245593267@qq.com>
2017-06-08 21:18:24 +0800

    ceshi

--- app/src/main/java/xueya/jiyun/com/xueya/Student.java
+++ app/src/main/java/xueya/jiyun/com/xueya/Student.java
@@ -8,6 +8,14 @@ public class Student {
     String name;
     String psw;
 
+    public String getPsw() {
+        return psw;
+    }
+
+    public void setPsw(String psw) {
+        this.psw = psw;
+    }
+
     public Student(String name) {
         this.name = name;
     }
