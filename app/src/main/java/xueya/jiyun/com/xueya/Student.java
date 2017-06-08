f132314bd1a64f15ce7cae0254fac83ef3c16920
Asus <945727250@>
2017-06-08 20:24:38 +0800

    测试

---
+++ app/src/main/java/xueya/jiyun/com/xueya/Student.java
@@ -0,0 +1,17 @@
+package xueya.jiyun.com.xueya;
+
+/**
+ * Created by Asus on 2017/6/8.
+ */
+
+public class Student {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
