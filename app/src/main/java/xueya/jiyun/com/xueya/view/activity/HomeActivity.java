bab20170d14e462bcc11d5badf1d1926bd83d73c
fengeryun <1945648435@qq.com>
2017-06-08 21:47:45 +0800

    cccc

--- app/src/main/java/xueya/jiyun/com/xueya/view/activity/HomeActivity.java
+++ app/src/main/java/xueya/jiyun/com/xueya/view/activity/HomeActivity.java
@@ -1,16 +1,15 @@
-package xueya.jiyun.com.xueya;
+package xueya.jiyun.com.xueya.view.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import xueya.jiyun.com.xueya.R;
+
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-        
+        setContentView(R.layout.activity_home);
     }
 }
---
+++ app/src/main/java/xueya/jiyun/com/xueya/view/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package xueya.jiyun.com.xueya.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import xueya.jiyun.com.xueya.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        
+    }
+}
