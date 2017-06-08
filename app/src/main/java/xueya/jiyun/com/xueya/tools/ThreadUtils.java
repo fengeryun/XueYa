94daa20d9aa2b0f65f4eb209cad964fe8fdb93c9
fengeryun <1945648435@qq.com>
2017-06-08 21:51:41 +0800

    ddd

---
+++ app/src/main/java/xueya/jiyun/com/xueya/tools/ThreadUtils.java
@@ -0,0 +1,22 @@
+package xueya.jiyun.com.xueya.tools;
+
+import android.os.Handler;
+import android.os.Looper;
+
+/**
+ *
+ * 线程切换
+ * Created by my on 2017/5/14.
+ */
+
+public class ThreadUtils {
+    private static Handler sHander = new Handler(Looper.getMainLooper());
+
+    public static void runOnMain(Runnable runnable) {
+        sHander.post(runnable);
+    }
+
+    public static void runOnSubThread(Runnable runnable) {
+        new Thread(runnable).start();
+    }
+}
