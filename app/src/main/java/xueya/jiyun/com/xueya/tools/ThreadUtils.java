4d8504a4dea891eeb3b2789dc1d7445f2a8324af
Asus <945727250@>
2017-06-09 11:01:26 +0800

    测试

--- app/src/main/java/xueya/jiyun/com/xueya/tools/ThreadUtils.java
+++ app/src/main/java/xueya/jiyun/com/xueya/tools/ThreadUtils.java
@@ -12,10 +12,12 @@ import android.os.Looper;
 public class ThreadUtils {
     private static Handler sHander = new Handler(Looper.getMainLooper());
 
+    //主线程
     public static void runOnMain(Runnable runnable) {
         sHander.post(runnable);
     }
 
+    //子线程
     public static void runOnSubThread(Runnable runnable) {
         new Thread(runnable).start();
     }
