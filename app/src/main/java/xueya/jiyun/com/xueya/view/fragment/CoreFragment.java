fd7486adfe1485feb79845a9bdb84a596ff91a2c
hyj1245593267 <1245593267@qq.com>
2017-06-09 18:15:35 +0800

    login

--- app/src/main/java/xueya/jiyun/com/xueya/view/fragment/CoreFragment.java
+++ app/src/main/java/xueya/jiyun/com/xueya/view/fragment/CoreFragment.java
@@ -1,6 +1,7 @@
 package xueya.jiyun.com.xueya.view.fragment;
 
 import android.view.View;
+import android.widget.LinearLayout;
 
 import xueya.jiyun.com.xueya.R;
 import xueya.jiyun.com.xueya.view.base.BaseFragment;
@@ -10,9 +11,16 @@ import xueya.jiyun.com.xueya.view.base.BaseFragment;
  */
 
 public class CoreFragment extends BaseFragment{
+    //                登录        加号        收藏        资料     消息  设置
+    LinearLayout mine_nologin,mine_sign,mine_collect,mine_data,mine_msg,mine_set;
     @Override
     public void initView(View view) {
-
+        mine_nologin= (LinearLayout) view.findViewById(R.id.mine_nologin);
+        mine_sign= (LinearLayout) view.findViewById(R.id.mine_sign);
+        mine_collect= (LinearLayout) view.findViewById(R.id.mine_collect);
+        mine_data= (LinearLayout) view.findViewById(R.id.mine_data);
+        mine_msg= (LinearLayout) view.findViewById(R.id.mine_msg);
+        mine_set= (LinearLayout) view.findViewById(R.id.mine_set);
     }
 
     @Override
