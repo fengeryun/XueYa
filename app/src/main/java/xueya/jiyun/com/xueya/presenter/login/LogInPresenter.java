5f933bd9a42d6ffa271aa2fefab766dbb891d667
hyj1245593267 <1245593267@qq.com>
2017-06-10 09:28:42 +0800

    login

--- app/src/main/java/xueya/jiyun/com/xueya/presenter/login/LogInPresenter.java
+++ app/src/main/java/xueya/jiyun/com/xueya/presenter/login/LogInPresenter.java
@@ -2,22 +2,43 @@ package xueya.jiyun.com.xueya.presenter.login;
 
 import android.text.TextUtils;
 
+import java.util.HashMap;
+
+import xueya.jiyun.com.xueya.model.callback.NewUrlCallback;
+import xueya.jiyun.com.xueya.model.http.VolleyUtils;
+
 /**
  * Created by 123 on 2017/6/9.
  */
 
 public class LogInPresenter implements ILogInPresenter {
+    private LogInView logInView;
+
+    public LogInPresenter(LogInView logInView) {
+        this.logInView = logInView;
+    }
 
     @Override
     public void login(String username, String psw) {
         if (TextUtils.isEmpty(username)) {
-
+            logInView.shouMessage("用户名不能为空");
             return;
         }
 
         if (TextUtils.isEmpty(psw)) {
-
+            logInView.shouMessage("密码不能为空");
             return;
         }
+        VolleyUtils.getInstance().doPost("", new HashMap<String, String>(), new NewUrlCallback() {
+            @Override
+            public void success(String eryun) {
+                logInView.startActivity();
+            }
+
+            @Override
+            public void error(int code, String erge) {
+                logInView.shouMessage("登录失败,请检查用户名和密码");
+            }
+        });
     }
 }
